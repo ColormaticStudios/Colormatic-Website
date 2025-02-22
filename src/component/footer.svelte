d3624273d775087dc93a3c09c6d23d0352cb4bab
Zakarya <zakarya@colormatic.org>
2025-02-22 14:51:30 -0800

    Footer: Add contact email

--- src/component/footer.svelte
+++ src/component/footer.svelte
@@ -1,4 +1,7 @@
 <footer>
   <p>© 2025 Colormatic Studios, All Rights Reserved.</p>
-  <p><a href="mailto:support@colormatic.org">support@colormatic.org</a></p>
+  <p>
+    <a href="mailto:support@colormatic.org">support@colormatic.org</a> |
+    <a href="mailto:support@colormatic.org">contact@colormatic.org</a>
+  </p>
 </footer>
