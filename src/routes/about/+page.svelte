bf419426cf8d85cbaa5663d9c1d2cfff2fb63f6f
Zakarya <zakarya@colormatic.org>
2025-03-18 21:33:02 -0700

    About page: add more text

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -8,12 +8,19 @@
   <div class="hero panel">
     <h1>Colormatic: A non-profit project for creation.</h1>
     <p class="justify">
-      Colormatic is a non-profit project for creating a curated collection of
-      sub-projects that match a high-quality, high attention to detail standard.
+      Colormatic is a non-profit project from Zakarya for creating a curated
+      collection of sub-projects that match a high-quality, high attention to
+      detail standard.
     </p>
     <p class="justify">
-      Colormatic Studios is a creative studio dedicated to giving life to these
-      maximum effort projects.
+      Colormatic is still in the early stages, so expect many things to change
+      in the near future.
+    </p>
+    <p class="justify">
+      Colormatic Studios is a creative studio dedicated to giving life to
+      Colormatic's projects. We are currently just a small group of passionate
+      volunteers working to build inspiring, intuitive and innovative creative
+      works.
     </p>
   </div>
 </main>
